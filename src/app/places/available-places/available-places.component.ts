49c4ddd981162ac6a70985ec99907b19938ad9df
Sai Krishna <kanapuramsaikrishna1999@gmail.com>
2026-09-23 17:26:41 +0530

    response transformation

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,9 +1,12 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/internal/operators/map';
+import { throwError } from 'rxjs/internal/observable/throwError';
+import { catchError } from 'rxjs/internal/operators/catchError';
 
 @Component({
   selector: 'app-available-places',
@@ -12,7 +15,40 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
-  private htttpClient = inject(HttpClient);
+  isFetchingPlaces = signal<boolean>(false);
+  error = signal<string>('');
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit() {
+    this.isFetchingPlaces.set(true);
+    const subscription = this.httpClient.get<{ places: Place[] }>('http://localhost:3000/places').pipe( map((res) => res.places), 
+    catchError((error) => {
+      return throwError(() => 
+        new Error(
+          'There is a technical issue while fetching the places. Please try again later.'
+        )
+      );
+    })).subscribe({
+      next: (places) => {
+        console.log('responseData', places);
+        this.places.set([...places]);
+      },
+      error: (error) => {
+        console.error('Error fetching places:', error);
+        this.error.set(error.message);
+      },
+      complete: () => {
+        this.isFetchingPlaces.set(false);
+      }
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+
+  }   
+  
 }
